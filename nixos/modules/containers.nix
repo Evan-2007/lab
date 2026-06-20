f47a55b316c7d4a8650e21fa349d3ea7a3801868
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2026-06-19 19:42:46 -0700

    fix: update KOMODO_OIDC_PROVIDER URL to remove the authorization path

--- nixos/modules/containers.nix
+++ nixos/modules/containers.nix
@@ -94,7 +94,7 @@
         KOMODO_LOCAL_AUTH = "true";
         KOMODO_ENABLE_NEW_USERS = "false";
         KOMODO_OIDC_ENABLED = "true";
-        KOMODO_OIDC_PROVIDER = "https://id.lab.evanc.dev/authorize";
+        KOMODO_OIDC_PROVIDER = "https://id.lab.evanc.dev";
         KOMODO_OIDC_CLIENT_ID = "ba2fb233-26f7-46cf-80c9-14cfe7474bd4";
       };
     };
